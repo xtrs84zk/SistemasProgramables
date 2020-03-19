e40c75a2aa28ea8f54387fce65ebac610272c5ce
Javier Sanchez <xtrs84zk@gmail.com>
2020-03-18 18:34:29 -0600

    Documentación.

--- Practica20/Practica20.ino
+++ Practica20/Practica20.ino
@@ -1,20 +1,21 @@
-//El botón presionado envía 0V (LOW)
-//El botón no presionado envía 5V
-//(input-pullup) implica una resistencia "interna" ergo,
-//no se necesita una física, esto en el pinMode.
+/** El botón presionado envía 0V (LOW)
+El botón no presionado envía 5V
+(input-pullup) implica una resistencia "interna" ergo,
+no se necesita una física, esto en el pinMode.
+**/
 void setup() {
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
+  //establecer los pin a utilizar como salida para los led
+  pinMode(6,OUTPUT); //led izquierdo
+  pinMode(7,OUTPUT); //led derecho
   pinMode(8,INPUT_PULLUP);//se activa la resistencia interna
 }
 
 void loop() {
-  if(digitalRead(8) == LOW){ //presionado
-    digitalWrite(6,HIGH);
-    digitalWrite(7,LOW);
-  } else {
-    digitalWrite(6,LOW);
-    digitalWrite(7,HIGH);
+  if(digitalRead(8) == LOW){ //botón presionado
+    digitalWrite(6,HIGH); //enciende led izquierdo
+    digitalWrite(7,LOW); //apaga led derecho
+  } else { //botón "al aire"
+    digitalWrite(6,LOW); //apaga led izquierdo
+    digitalWrite(7,HIGH); //enciende led derecho
   }
-
 }
